5ad7dc37cefdc96b5777c25e6f1e68713d5dafbd
Jhordy Gavinchu <dev@jhordyess.com>
2023-04-13 21:22:29 +0000

    Improve code readability ⚒

--- history.sh
+++ history.sh
@@ -1,14 +1,18 @@
 #? Command history
-sudo npm i npm -g
-# npm init -y
+# Install global npm package for npm
+sudo npm i -g npm
+
+# Install required packages for React development
 npm i react react-dom
+
+# Bundler
 npm i -D webpack webpack-cli webpack-dev-server
 npm i -D html-webpack-plugin
 npm i -D mini-css-extract-plugin css-loader style-loader
-
-npm i -D @types/react @types/react-dom
-npm i -D typescript ts-loader
-
 npm i -D css-minimizer-webpack-plugin terser-webpack-plugin clean-webpack-plugin
 
+# Hosting
 npm i -D gh-pages
+
+# Extra commands
+npm i -D typescript ts-loader @types/react @types/react-dom
